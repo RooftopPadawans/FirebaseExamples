752769a287ea868f0fe276ec7d15d76ce852d765
David Perez <dpereze1105@hotmail.com>
2022-12-01 07:52:45 -0600

    Adding Realtime DB

--- app/src/main/java/com/flknlabs/firebaseexamples/MainActivity.kt
+++ app/src/main/java/com/flknlabs/firebaseexamples/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.flknlabs.firebaseexamples
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.google.firebase.FirebaseApp
-import com.google.firebase.firestore.CollectionReference
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val db = Firebase.firestore
+    private val database = Firebase.database
 
     companion object {
         val TAG = "MainActivity"
@@ -23,9 +26,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnSaveHardcoded.setOnClickListener { saveHardcoded() }
+        btnSaveHardcoded.setOnClickListener { readFromRealtime() }
     }
 
+    // Saving with Firestore
     private fun saveHardcoded() {
         val user = hashMapOf(
             "name" to "Ada Ava",
@@ -92,11 +96,62 @@ class MainActivity : AppCompatActivity() {
                 callback(null)
             }
     }
+
+    // Saving with Realtime DB
+    private fun saveRealTime() {
+        /*val myRef = database.getReference("message")
+        myRef.setValue("Hello, World!")*/
+
+        /*val student = Student(
+            name = "David Perez",
+            age = 29,
+            isStudent = false,
+        )
+        val newRef = database.getReference("student")
+        newRef.setValue(student)*/
+
+        val teachers = listOf<Teacher>(
+            Teacher("Martin del Campo", 32, true),
+            Teacher("Marcelina Gutierrez", 30,false)
+        )
+        val newRef2 = database.getReference("teachers")
+        newRef2.setValue(teachers)
+    }
+
+    private fun readFromRealtime() {
+        //val myRef = database.getReference("message")
+        //val newRef = database.getReference("student")
+        val newRef2 = database.getReference("teachers")
+
+        newRef2.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.value
+                Log.d(TAG, "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+    }
 }
 
 data class Student(
+    val id: Int = Utils.generateRandomID(),
     val name: String,
     val age: Int,
     @SerializedName("student")
     val isStudent: Boolean
 )
+
+data class Teacher(
+    val name: String,
+    val age: Int,
+    @SerializedName("active")
+    val isActive: Boolean
+)
+
+object Utils {
+    fun generateRandomID(): Int { return Random().nextInt(100000) }
+}
