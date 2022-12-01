ebafc272c9c746f5414d7dc50eba292ade672009
David Perez <dpereze1105@hotmail.com>
2022-12-01 08:56:55 -0600

    Adding Crashlitics

--- app/src/main/java/com/flknlabs/firebaseexamples/MainActivity.kt
+++ app/src/main/java/com/flknlabs/firebaseexamples/MainActivity.kt
@@ -2,10 +2,32 @@ package com.flknlabs.firebaseexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.Firebase
+import java.lang.Exception
+import java.lang.RuntimeException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val crashButton = Button(this)
+        crashButton.text = "Test Crash"
+        crashButton.setOnClickListener {
+            try {
+                val result = 1 / 0
+                Toast.makeText(this, "Result is $result", Toast.LENGTH_SHORT).show()
+            } catch(e: Exception) {
+                Toast.makeText(this, "Cannot divide by zero", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
     }
 }
